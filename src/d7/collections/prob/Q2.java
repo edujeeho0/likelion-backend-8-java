1fb045a7bc7925c95ddc01a2177795b8ae9a1611
edujeeho0 <edujeeho@gmail.com>
2023-11-29 11:47:04 +0900

    optional + collection (11월 29일 오전)

---
+++ src/d7/collections/prob/Q2.java
@@ -0,0 +1,13 @@
+package d7.collections.prob;
+
+/*
+엔터로 구분된 정수 입력이 임의의 횟수만큼 이뤄진다
+`q`가 입력되면 모든 데이터가 입력된 것이라고 생각할 때,
+여태까지 입력한 정수가 몇가지가 되는지 출력하시오.
+Integer.parseInt(String str)
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+}
